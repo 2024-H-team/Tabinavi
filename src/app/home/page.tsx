583ba3517d50c8ef9f6756e6fbe1e5719c3ccc3c
Soujirou <soujirou.takagi@gmail.com>
2024-11-04 23:43:51 +0900

    ScheduleViewコンポーネントの作成

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,18 @@
 import ScheduleView from '@/components/Home/ScheduleView';
+import style from '@styles/appStyles/home.module.scss';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <ScheduleView />
+        <div className={style.ScheduleWrap}>
+            <h2 style={{ fontSize: '16px' }}>直近の予定</h2>
+            <ScheduleView
+                month={1}
+                day={10}
+                TimeStart="10:00"
+                TimeEnd="22:30"
+                Title="直近の予定直近の予定"
+                SubTitle="直近の予定直近の予定"
+            />
         </div>
     );
 }
--- src/components/Home/ScheduleView.tsx
+++ src/components/Home/ScheduleView.tsx
@@ -1,3 +1,34 @@
-export default function ScheduleView() {
-    return <div>ScheduleView</div>;
+import React from 'react';
+import ScheduleDate from '@/components/Home/ScheduleDate';
+import style from '@styles/componentStyles/ScheduleView.module.scss';
+import { BiChevronRight } from 'react-icons/bi';
+
+interface ScheduleDateProps {
+    month: number;
+    day: number;
+    Title: string;
+    SubTitle: string;
+    TimeStart: string;
+    TimeEnd: string;
+}
+
+export default function ScheduleView({ month, day, SubTitle, Title, TimeStart, TimeEnd }: ScheduleDateProps) {
+    return (
+        <div className={style.ScheduleWrap}>
+            <div>
+                <ScheduleDate month={month} day={day} />
+            </div>
+
+            <div className={style.ScheduleContents}>
+                <div className={style.ScheduleInformation}>
+                    <p className={style.time}>
+                        {TimeStart}~{TimeEnd}
+                    </p>
+                    <h3>{Title}</h3>
+                    <h4>{SubTitle}</h4>
+                </div>
+                <BiChevronRight size="30px" color="blue" />
+            </div>
+        </div>
+    );
 }
