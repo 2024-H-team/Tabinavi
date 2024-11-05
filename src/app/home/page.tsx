a5ee105ad224867d67eee603488c4fc73753e57d
Soujirou <soujirou.takagi@gmail.com>
2024-11-05 15:27:09 +0900

    カレンダー枠のみ作成

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,18 +1,58 @@
 import ScheduleView from '@/components/Home/ScheduleView';
 import style from '@styles/appStyles/home.module.scss';
 
+// const ScheduleViewArray = [
+//     [
+//         {
+//             month: 1,
+//             day: 10,
+//             TimeStart: '10:00',
+//             TimeEnd: '22:30',
+//             Title: '直近の予定直近の予定',
+//             SubTitle: '直近の予定直近の予定',
+//         },
+//         {
+//             month: 1,
+//             day: 11,
+//             TimeStart: '11:00',
+//             TimeEnd: '23:30',
+//             Title: '直近の予定直近の予定2',
+//             SubTitle: '直近の予定直近の予定2',
+//         },
+//     ],
+// ];
+
 export default function Home() {
     return (
-        <div className={style.ScheduleWrap}>
-            <h2 style={{ fontSize: '16px' }}>直近の予定</h2>
-            <ScheduleView
-                month={1}
-                day={10}
-                TimeStart="10:00"
-                TimeEnd="22:30"
-                Title="直近の予定直近の予定"
-                SubTitle="直近の予定直近の予定"
-            />
-        </div>
+        <>
+            <div className={style.calendar}></div>
+            <div className={style.ScheduleWrap}>
+                <h2 style={{ fontSize: '16px' }}>直近の予定</h2>
+                <ScheduleView
+                    month={1}
+                    day={10}
+                    TimeStart="10:00"
+                    TimeEnd="22:30"
+                    Title="直近の予定直近の予定"
+                    SubTitle="直近の予定直近の予定"
+                />
+                <ScheduleView
+                    month={1}
+                    day={10}
+                    TimeStart="10:00"
+                    TimeEnd="22:30"
+                    Title="直近の予定直近の予定"
+                    SubTitle="直近の予定直近の予定"
+                />
+                <ScheduleView
+                    month={1}
+                    day={10}
+                    TimeStart="10:00"
+                    TimeEnd="22:30"
+                    Title="直近の予定直近の予定"
+                    SubTitle="直近の予定直近の予定"
+                />
+            </div>
+        </>
     );
 }
