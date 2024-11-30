9ee05b966e87af6751672f3c11f974521c12aba1
sojiro takagi <soujirou.takagi@gmail.com>
2024-11-30 17:34:21 +0900

    持ち物欄をコンポーネントに変更

--- client/src/components/edit/Content.tsx
+++ client/src/components/edit/Content.tsx
@@ -1,7 +1,5 @@
 import styles from '@styles/componentStyles/edit/Content.module.scss';
-import { MdClose } from 'react-icons/md';
-import { HiOutlinePencil } from 'react-icons/hi2';
-import EditField from './EditField';
+import EditField, { EditFieldPE } from './EditField';
 
 type ScheduleData = {
     id: number;
@@ -20,27 +18,7 @@ export default function Content({ data }: ContentProps) {
         <div className={styles.contentWrap}>
             <EditField title="旅行先" value={data?.Location || '未設定'} />
             <EditField title="滞在時間" value={data?.Time + '時間' || '未設定'} />
-
-            <div className={styles.editWrap}>
-                <div>
-                    <h2>持ち物</h2>
-                    {data?.PersonalEffects.map((effect, index) => {
-                        return (
-                            <div key={index} className={styles.PersonalEffects}>
-                                <p>
-                                    {effect.Name}
-                                    <button>
-                                        <MdClose size="12" />
-                                    </button>
-                                </p>
-                            </div>
-                        );
-                    })}
-                </div>
-                <button className={styles.editBtn}>
-                    <HiOutlinePencil color="#929292" />
-                </button>
-            </div>
+            <EditFieldPE title="持ち物" data={data} />
             <div className={styles.editWrap}>
                 <h2>メモ</h2>
                 <p></p>
--- client/src/components/edit/EditField.tsx
+++ client/src/components/edit/EditField.tsx
@@ -1,5 +1,6 @@
 import styles from '@styles/componentStyles/edit/Content.module.scss';
 import { HiOutlinePencil } from 'react-icons/hi2';
+import { MdClose } from 'react-icons/md';
 import { useState } from 'react';
 
 type EditFieldProps = {
@@ -25,6 +26,36 @@ export default function EditField({ title, value }: EditFieldProps) {
                 <input type="text" value={value} readOnly={!isEditable} onBlur={handleBlur} />
             </div>
             <button className={styles.editBtn} onClick={handleEnableEdit}>
+                <HiOutlinePencil color="#929292" />
+            </button>
+        </div>
+    );
+}
+
+type EditFieldPEProps = {
+    title: string;
+    data: { PersonalEffects: { Name: string }[] } | undefined;
+};
+
+export function EditFieldPE({ title, data }: EditFieldPEProps) {
+    return (
+        <div className={styles.editWrap}>
+            <div>
+                <h2>{title}</h2>
+                {data?.PersonalEffects.map((effect, index) => {
+                    return (
+                        <div key={index} className={styles.PersonalEffects}>
+                            <p>
+                                {effect.Name}
+                                <button>
+                                    <MdClose size="12" />
+                                </button>
+                            </p>
+                        </div>
+                    );
+                })}
+            </div>
+            <button className={styles.editBtn}>
                 <HiOutlinePencil color="#929292" />
             </button>
         </div>
