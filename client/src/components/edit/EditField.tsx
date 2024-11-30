035c548227d37d4754fbb0b1171956a479ddefa1
sojiro takagi <soujirou.takagi@gmail.com>
2024-11-30 18:22:46 +0900

    ボタンを押した時、旅行先と滞在時間を変更できるように

--- client/src/components/edit/EditField.tsx
+++ client/src/components/edit/EditField.tsx
@@ -1,7 +1,7 @@
 import styles from '@styles/componentStyles/edit/Content.module.scss';
 import { HiOutlinePencil } from 'react-icons/hi2';
 import { MdClose } from 'react-icons/md';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 type EditFieldProps = {
     title: string;
@@ -10,20 +10,40 @@ type EditFieldProps = {
 
 export default function EditField({ title, value }: EditFieldProps) {
     const [isEditable, setIsEditable] = useState(false);
+    const [inputValue, setInputValue] = useState(value);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        setInputValue(value);
+    }, [value]);
 
     const handleEnableEdit = () => {
         setIsEditable(true);
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
     };
 
     const handleBlur = () => {
         setIsEditable(false);
     };
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value);
+    };
+
     return (
         <div className={styles.editWrap}>
             <div>
                 <h2>{title}</h2>
-                <input type="text" value={value} readOnly={!isEditable} onBlur={handleBlur} />
+                <input
+                    ref={inputRef}
+                    type="text"
+                    value={inputValue}
+                    readOnly={!isEditable}
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                />
             </div>
             <button className={styles.editBtn} onClick={handleEnableEdit}>
                 <HiOutlinePencil color="#929292" />
