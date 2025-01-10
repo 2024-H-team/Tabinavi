f73600824000364da1a40525cc231bf14b63cb7e
sojiro takagi <soujirou.takagi@gmail.com>
2025-01-10 11:38:48 +0900

    CalenderDataをfetch

--- client/src/components/home/ScheduleView.tsx
+++ client/src/components/home/ScheduleView.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ScheduleDate from '@/components/home/ScheduleDate';
 import styles from '@styles/componentStyles/home/ScheduleView.module.scss';
 import { BiChevronRight } from 'react-icons/bi';
 import ScheduleViewArray from '@/utils/ScheduleViewArray';
 
+interface Schedule {
+    id: number;
+    startDate: string;
+    endDate: string;
+    StartTime: string;
+    EndTime: string;
+    Title: string;
+    SubTitle: string;
+}
+
 export default function ScheduleView() {
+    const [schedule, setSchedule] = React.useState<Schedule[]>([]);
+
+    useEffect(() => {
+        const fetchSchedule = async () => {
+            const response = await fetch('/schedule.json');
+            if (!response.ok) {
+                throw new Error(`Failed to fetch schedule: ${response.status}`);
+            }
+
+            const data: Schedule[] = await response.json();
+
+            setSchedule(data);
+        };
+        fetchSchedule();
+    }, []);
+
     return (
         <>
             <h2 style={{ fontSize: '16px' }}>直近の予定</h2>
