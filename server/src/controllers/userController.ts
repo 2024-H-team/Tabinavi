f3afade36a3528c8756fac34c7607af57e017820
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2025-01-13 13:43:07 +0900

    Update cookie settings in UserController for enhanced security

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -105,9 +105,9 @@ export class UserController {
             // Set cookie
             res.cookie('token', token, {
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
+                secure: true,
                 maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-                sameSite: 'lax',
+                sameSite: 'none',
                 path: '/',
             });
 
